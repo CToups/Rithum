78305fd20156d3279628654d43e77d21bbe25651
Chris Toups <Ctoups89@gmail.com>
2015-12-02 18:56:16 -0600

    Rithum Updated
    Have good quality. Have separate file’s depending on memo or audio
    recording. Good quality for audio. Bad for memo. Have shorter toast
    messages. Changed toast message based on recording or playback.

--- RithumDemo/app/src/main/java/com/example/ct/audiocapture/MainActivity.java
+++ RithumDemo/app/src/main/java/com/example/ct/audiocapture/MainActivity.java
@@ -39,6 +39,15 @@ public class MainActivity extends Activity {
     private String outputFile = null;
     private String outputTemp = null;
 
+    private boolean currentlyRecording = false;
+
+    /*
+    File vPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+            + "/Android/data/Rithum-VoiceMemo's/");
+    File mPath = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+            + "/Android/data/Rithum-MusicRecordings/");
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +58,15 @@ public class MainActivity extends Activity {
         //stop=(Button)findViewById(R.id.button2);
         //record=(Button)findViewById(R.id.button);
 
+        //Creates the two directories where audio files will be saved
+        File vDir = new File(Environment.getExternalStorageDirectory()+
+                File.separator+"Rithum_Memo's");
+        vDir.mkdirs();
+        File aDir = new File(Environment.getExternalStorageDirectory()+
+                File.separator+"Rithum_Audio");
+        aDir.mkdirs();
+
+
         //Necessary to get an image as button
         record=(ImageButton)findViewById(R.id.myButton);
         play=(ImageButton)findViewById(R.id.myPlayButton);
@@ -58,23 +76,28 @@ public class MainActivity extends Activity {
         stop.setEnabled(false);
         play.setEnabled(false);
 
+        //getRecordingType();
+
         getFileName();
 
         myAudioRecorder=new MediaRecorder();
         myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        //myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        //myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        //myAudioRecorder.setAudioEncoder(MediaRecorder.getAudioSourceMax());
+
 
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                currentlyRecording = true;
+
                 //ImageButton Stuff
                 record.setImageResource(R.drawable.recbuttenabled);
                 play.setImageResource(R.drawable.playbuttenabled);
-
-
-
 
                 try {
                     myAudioRecorder.prepare();
@@ -94,7 +117,7 @@ public class MainActivity extends Activity {
                 record.setEnabled(false);
                 stop.setEnabled(true);
                 play.setEnabled(false);
-                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -114,19 +137,29 @@ public class MainActivity extends Activity {
                 else {
                     myAudioRecorder.stop();
                     myAudioRecorder.reset();
-                    //myAudioRecorder.release();
-                    //myAudioRecorder  = null;
 
                     //Boiler plate to allow us to record over existing track OR play track upon stop
                     myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                    myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-
-                    Toast.makeText(getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
+                    //myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                    myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                    //myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+                    myAudioRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                    //myAudioRecorder.setAudioEncoder(MediaRecorder.getAudioSourceMax());
                 }
                 stop.setEnabled(false);
                 play.setEnabled(true);
                 record.setEnabled(true); //originally set to false
+
+
+                //Trying to send "audio recorded successfully" message
+                if (currentlyRecording){
+                    Toast.makeText(getApplicationContext(), "Track Successfully Recorded",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Stopped",Toast.LENGTH_SHORT).show();
+                }
+                currentlyRecording = false;
 
             }
         });
@@ -134,6 +167,8 @@ public class MainActivity extends Activity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) throws IllegalArgumentException,SecurityException,IllegalStateException {
+
+                currentlyRecording = false;
 
                 //ImageButton Stuff
                 play.setImageResource(R.drawable.playbuttenabled);
@@ -164,7 +199,7 @@ public class MainActivity extends Activity {
                 }
 
                 m.start();
-                Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -172,6 +207,21 @@ public class MainActivity extends Activity {
     public double getAmplitude(){
         return myAudioRecorder.getMaxAmplitude() /100;
     }
+
+
+    public void getRecordingType(){
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Type Of Recording");
+
+        alert.setPositiveButton("Voice Memo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                //outputFile = vPath;
+            }
+        });
+    }
+
 
     public void getFileName(){
         //Helper methods to get input for output filename
@@ -184,7 +234,7 @@ public class MainActivity extends Activity {
         final EditText input = new EditText(this);
         alert.setView(input);
 
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        alert.setPositiveButton("Memo (low quality)", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 outputTemp = input.getText().toString();
 
@@ -192,21 +242,49 @@ public class MainActivity extends Activity {
                 Log.w("audiocapture", outputTemp);
 
                 //Crazy Test
-                outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + outputTemp + ".3gp";
+                // outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + outputTemp + ".3gp";
+                // myAudioRecorder.setOutputFile(outputFile);
+
+                outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/Rithum_Memo's/" + outputTemp + ".mp4";
+                myAudioRecorder.setOutputFile(outputFile);
+            }
+        });
+
+
+        alert.setNeutralButton("Music (high quality)", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                outputTemp = input.getText().toString();
+
+
+                //Change Audio Quality (for the better)
+                myAudioRecorder.setAudioEncodingBitRate(128000);
+                myAudioRecorder.setAudioSamplingRate(44100);
+
+
+                //Ok so outputTemp is definitely being stored properly, this log proves it
+                Log.w("audiocapture", outputTemp);
+
+                //Crazy Test
+                // outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + outputTemp + ".3gp";
+                // myAudioRecorder.setOutputFile(outputFile);
+
+                outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/Rithum_Audio/" + outputTemp + ".mp4";
                 myAudioRecorder.setOutputFile(outputFile);
             }
         });
 
         alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
-                // Canceled.
+                //
+                System.exit(0);
+                Log.w("audiocapture", "user hit cancel");
             }
         });
 
         alert.show();
     }
-
-
 
 
     @Override
